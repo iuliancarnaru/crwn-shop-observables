9af0f6f06418bf5178ca4e07a53eb5a7013c724a
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-14 16:43:43 +0100

    persisting store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+// localStorage
+import storage from "redux-persist/lib/storage";
+
+// because user is handled by Firebase we pass only cart
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
