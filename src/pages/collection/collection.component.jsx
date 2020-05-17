a52313e225452b2967e93b1f6cb7ef44c64aff3d
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-17 13:18:28 +0100

    adding category page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./collection.styles.scss";
+import CollectionItem from "../../components/collection-item";
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>Category page</h2>categorycategory
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collections-overview";
+import CollectionPage from "../collection";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
