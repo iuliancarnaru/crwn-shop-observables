cb7001c79afca61ec0b44a38630d8c52bced9d43
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-18 17:31:26 +0100

    updating shop selectors

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./collection.styles.scss";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
 import CollectionItem from "../../components/collection-item";
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => (
   <div className="collection-page">
     <h2>Category page</h2>categorycategory
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
