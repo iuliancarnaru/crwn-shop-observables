caad8ffbc795a3598b1236ad1206e8537db0773a
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-19 09:50:23 +0100

    adding stripe and updating favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -34,13 +34,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
     <div className="total">
       <div>TOTAL: ${total}</div>
-      <div className="test-warning">
-        *Please use the following test credit card for payments*
-        <br />
-        4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
-      </div>
-      <StripeCheckoutButton price={total} />
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
