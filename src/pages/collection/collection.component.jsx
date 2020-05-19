ebdeac665b8c1bc9765fa2415ff92197f22ea82e
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-19 06:38:08 +0100

    normalizing data

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,11 +5,19 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import CollectionItem from "../../components/collection-item";
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection-page">
-    <h2>Category page</h2>categorycategory
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,12 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+// const COLLECTION_ID_MAP = {
+//   hats: 1,
+//   sneakers: 2,
+//   jackets: 3,
+//   womens: 4,
+//   mens: 5,
+// };
 
 const selectShop = (state) => state.shop;
 
@@ -15,9 +15,17 @@ export const selectShopCollections = createSelector(
   (shop) => shop.collections
 );
 
+// when data was array
+// export const selectCollection = (collectionUrlParam) =>
+//   createSelector([selectShopCollections], (collections) =>
+//     collections.find(
+//       (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   );
+
+// after data normalisation (converted to object)
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectShopCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectShopCollections],
+    (collections) => collections[collectionUrlParam]
   );
