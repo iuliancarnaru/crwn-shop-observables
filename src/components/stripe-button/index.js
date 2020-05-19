06f57fa23146404e2a27770bd6b6cff790f552bf
iuliancarnaru <iulian.carnaru@gmail.com>
2020-05-19 08:17:02 +0100

    adding stripe modal (to be updated)

---
+++ src/components/stripe-button/index.js
@@ -0,0 +1,1 @@
+export { default } from "./stripe-button.component";
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,7 @@ import {
   selectCartTotal,
 } from "../../redux/cart/cart.selectors";
 import CheckoutItem from "../../components/checkout-item";
+import StripeCheckoutButton from "../../components/stripe-button";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -32,7 +33,13 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">
-      <span>TOTAL: ${total}</span>
+      <div>TOTAL: ${total}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   </div>
 );
